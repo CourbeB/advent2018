f21b0f9074aa5bc5cc6d7113bc24a00338073e86
Baptiste Courbe <baptiste.courbe@MacBook-Pro-de-Baptiste.local>
2018-12-03 17:19:05 +0100

    ex12

--- src/main/scala/Advent.scala
+++ src/main/scala/Advent.scala
@@ -1,9 +1,11 @@
+import scala.collection.mutable
 import scala.io.Source
 
 object Advent {
 
   def main(args: Array[String]): Unit = {
     println(s"Result 1.1 : $exercise11")
+    println(s"Result 1.2 : $exercise12")
   }
 
   def exercise11(): Int ={
@@ -12,12 +14,23 @@ object Advent {
   }
 
   def exercise12(): Int ={
-    val in = Source.fromURL(getClass.getResource(s"/ex12.txt")).getLines().toList
-    val res = Set[Int]()
-    while (true){
-      
-    }
-    in.head
+    lazy val in = Source.fromURL(getClass.getResource(s"/ex12.txt"))
+      .getLines()
+      .map(_.toInt)
+      .toStream
+
+    val stream = Iterator.continually(in).flatten.scanLeft(0)(_ + _)
+    val res = mutable.Set[Int]()
+    var ft :Option[Int] = None
+
+    do {
+      val elem = stream.next
+      if(!res.add(elem)){
+        ft = Some(elem)
+      }
+    } while (ft.isEmpty)
+
+    ft.get
   }
 
 }
