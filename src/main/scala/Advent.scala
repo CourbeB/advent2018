e88cdec77485f7365aa541194eefb6ec30199104
baptiste.courbe <babacourbe@gmail.com>
2018-12-04 16:41:15 +0100

    ex21

--- src/main/scala/Advent.scala
+++ src/main/scala/Advent.scala
@@ -6,10 +6,12 @@ object Advent {
   def main(args: Array[String]): Unit = {
     println(s"Result 1.1 : $exercise11")
     println(s"Result 1.2 : $exercise12")
+
+    println(s"Result 2.1 : $exercise21")
   }
 
   def exercise11(): Int ={
-    val in = Source.fromURL(getClass.getResource(s"/ex11.txt")).getLines().toList
+    lazy val in = Source.fromURL(getClass.getResource(s"/ex11.txt")).getLines().toList
     in.map(_.toInt).sum
   }
 
@@ -31,6 +33,18 @@ object Advent {
     } while (ft.isEmpty)
 
     ft.get
+  }
+
+  def exercise21(): Int ={
+
+    implicit def toInt(b: Boolean): Int = if (b) 1 else 0
+
+    lazy val in = Source.fromURL(getClass.getResource(s"/ex21.txt")).getLines().toList
+    val tmp = in.map(_.groupBy(identity).mapValues(_.size).values.toSeq)
+    val (sumNb2, sumNb3) = tmp.foldLeft(0, 0){
+      case ((nb2, nb3), e) => (nb2 + e.contains(2), nb3 + e.contains(3))
+    }
+    sumNb2 * sumNb3
   }
 
 }
